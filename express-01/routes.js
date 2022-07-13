00bf0c258826699dc3399b05c60792bdc194230a
wesley almeida <obilhao@gmail.com>
2022-07-13 16:44:54 -0300

    add: ejs for renderizer

--- express-01/routes.js
+++ express-01/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
-const homePage = require('./controllers/homeController');
-const contats = require('./controllers/contatsController')
+const homePage = require('./src/controllers/homeController');
+const contats = require('./src/controllers/contatsController')
 
 route.get('/',homePage.homePage);
 route.post('/', homePage.firstPost)
