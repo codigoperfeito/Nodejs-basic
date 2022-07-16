3f608824d7d3c7d7cdff90781e6f4e4bbce64cc0
wesley almeida <obilhao@gmail.com>
2022-07-15 23:03:35 -0300

    add: models

--- model-express/src/controllers/homeController.js
+++ model-express/src/controllers/homeController.js
@@ -1,3 +1,11 @@
+const HomeModel = require('../models/HomeModel')
+
+HomeModel.create({
+    titulo: 'Segundo titulo enviado',
+    descicao: 'terceiro teste enviado'
+}).then((data)=>console.log(data))
+.catch((e)=>console.log('Erro: ',e))
+
 exports.homePage = (req,res)=> {
     res.render('index');
 }
